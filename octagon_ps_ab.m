e16575341fa8464d196aeb1a7f3d5559157ce546
karpenka <osnerde@gmail.com>
2018-04-27 18:43:59 +0300

    Add files via upload

---
+++ octagon_ps_ab.m
@@ -0,0 +1,87 @@
+function octagon_ps_ab(lambda,varargin)
+%parameters varargin -> f = -2, m = 6, ncount = 1e5
+numvarargs = length(varargin);
+if numvarargs > 3
+    error('myfuns:TooManyInputs', ...
+        'requires at most 3 optional inputs');
+end
+% set defaults for optional inputs
+optargs = {6 1e5};
+% now put these defaults into the valuesToUse cell array, 
+% and overwrite the ones specified in varargin.
+optargs(1:numvarargs) = varargin;
+% or ...
+% [optargs{1:numvarargs}] = varargin{:};
+
+% Place optional args in memorable variable names
+[m, ncount] = optargs{:};
+%guide parameters for geks function)
+%if you want to change something, look for a_max,a_min,a_step(for size of
+%octagon) and the same for L - length of the guide and you need change dir
+%in mcstas part of the function
+
+%guide parameters for geks function
+w = 0.03;
+h = 0.1;
+L1 = 40;
+L0 = 40;
+% a = 0.15;
+% b = a/sqrt(2);
+a_min = 0.01;
+a_step = 0.02;
+a_max = 0.21;
+b_min = 0.01;
+b_step = 0.02;
+b_max = 0.21;
+i=1;
+j=1;
+%Screw param
+%loop
+k=1;
+l=1;
+lr=1;
+rect(w,h,2*L0+L1);
+[p0,m0]=mcstas('screw_str.instr',struct('lambda',lambda,'L',2*L0+L1,'guide_m',m,'w',w,'h',h),struct('ncount',ncount,'mpi',4));
+p0l=p0(1,:,:).Signal;
+p0m=p0(2,:,:).Signal;
+p0s=p0(3,:,:).Signal;
+I0l(lr) = p0l;
+I0m(lr) = p0m;
+I0s(lr) = p0s;
+
+progressbar('a','b');
+for a = a_min:a_step:a_max
+for b = b_min:b_step:b_max
+    pause(0.0001)
+    geks_ps(a,b,w,h,L0,L1);
+    [p1,m1]=mcstas('LIRA_oct.instr',struct('lambda',lambda,'L0',L0,'L1',L1,'guide_m',m,'w',w,'h',h),struct('ncount',ncount,'mpi',4));
+    p1l=p1(1,:,:).Signal;
+    p1m=p1(2,:,:).Signal;
+    p1s=p1(3,:,:).Signal;
+    Il(l,k)=p1l/I0l;
+    Im(l,k)=p1m/I0m;
+    Is(l,k)=p1s/I0s;
+    frac1 = ((a-a_min)/a_step+1)/((a_max-a_min)/a_step+1);
+    frac2 = ((b-b_min)/b_step+1)/((b_max-b_min)/b_step+1);
+    progressbar(frac1, frac2);
+    l=l+1;
+end
+    k=k+1;
+    l=1;
+end
+
+dlmwrite(join([join(['oct_ab','lambda',string(lambda),'l'],'_'),'.dat'],''),Il,' ');
+dlmwrite(join([join(['oct_ab','lambda',string(lambda),'m'],'_'),'.dat'],''),Im,' ');
+dlmwrite(join([join(['oct_ab','lambda',string(lambda),'s'],'_'),'.dat'],''),Is,' ');
+aa=a_min:a_step:a_max;
+bb=b_min:b_step:b_max;
+[X,Y]=meshgrid(aa,bb);
+figure;
+surf(X,Y,Il)
+savefig(join(['oct_ab','lambda',string(lambda),'l'],'_'));
+figure;
+surf(X,Y,Im)
+savefig(join(['oct_ab','lambda',string(lambda),'m'],'_'));
+figure;
+surf(X,Y,Is)
+savefig(join(['oct_ab','lambda',string(lambda),'s'],'_'));
--- screw_mcstas.m
+++ screw_mcstas.m
@@ -1,27 +1,44 @@
-function screw_mcstas()
-%if you want to change something, look for a_max,a_min,a_step(for size of
-%octagon) and the same for L - length of the guide and you need change dir
-%in mcstas part of the function
+function screw_mcstas(lambda,varargin)
+%parameters varargin -> f = -2, m = 6, ncount = 1e5
+numvarargs = length(varargin);
+if numvarargs > 3
+    error('myfuns:TooManyInputs', ...
+        'requires at most 3 optional inputs');
+end
+% set defaults for optional inputs
+optargs = {-2 6 1e5};
+% now put these defaults into the valuesToUse cell array, 
+% and overwrite the ones specified in varargin.
+optargs(1:numvarargs) = varargin;
+% or ...
+% [optargs{1:numvarargs}] = varargin{:};
 
+% Place optional args in memorable variable names
+[f, m, ncount] = optargs{:};
 %guide parameters for geks function
-w=0.03;
-h=0.2;
-f=-2;
-i=1;
-j=1;
+w = 0.03;
+h = 0.1;
+i = 1;
+j = 1;
 %loop
-lr=1;
+lr = 1;
 L_min = 10/45;
 L_step = 5/45;
 L_max = 100/45;
 for L = L_min:L_step:L_max
     screw(w,h,L,f);
     rect(w,h,L*45);
-    [p0,m0]=mcstas('screw_str.instr',struct('L',L*45,'guide_m',6,'w',0.03,'h',0.2),struct('ncount',1e5));
-    [p1,m1]=mcstas('screw.instr',struct('L',L,'guide_m',6,'w',0.03,'h',0.2,'phi',f),struct('ncount',1e5));
-    p0=p0.Signal;
-    p1=p1.Signal;
-    I(lr) = p1/p0;
+    [p0,m0]=mcstas('screw_str.instr',struct('L',L*45,'lambda',lambda,'guide_m',m,'w',w,'h',h),struct('ncount',ncount,'mpi',4));
+    [p1,m1]=mcstas('screw.instr',struct('L',L,'lambda',lambda,'guide_m',m,'w',w,'h',h,'phi',f),struct('ncount',ncount,'mpi',4));
+    p0l=p0(1,:,:).Signal;
+    p1l=p1(1,:,:).Signal;
+    Il(lr) = p1l/p0l;
+    p0m=p0(2,:,:).Signal;
+    p1m=p1(2,:,:).Signal;
+    Im(lr) = p1m/p0m;
+    p0s=p0(3,:,:).Signal;
+    p1s=p1(3,:,:).Signal;
+    Is(lr) = p1s/p0s;
     lr=lr+1;
 end
 % phi_min = 0;
@@ -38,11 +55,21 @@ end
 %     I(lr) = p1/p0;
 %     lr=lr+1;
 % end
-dlmwrite('screw.dat',I,' ');
 % f = phi_min:phi_step:phi_max;
 % plot(f,I);
 L = L_min:L_step:L_max;
-plot(L,I)
+dlmwrite(join([join(['screw','lambda',string(lambda),'l'],'_'),'.dat'],''),Il,' ');
+dlmwrite(join([join(['screw','lambda',string(lambda),'m'],'_'),'.dat'],''),Im,' ');
+dlmwrite(join([join(['screw','lambda',string(lambda),'s'],'_'),'.dat'],''),Is,' ');
+figure;
+plot(L,Il)
+savefig(join(['screw','lambda',string(lambda),'l'],'_'));
+figure;
+plot(L,Im)
+savefig(join(['screw','lambda',string(lambda),'m'],'_'));
+figure;
+plot(L,Is)
+savefig(join(['screw','lambda',string(lambda),'s'],'_'));
 % aa=a_min:a_step:a_max;
 % [X,Y]=meshgrid(aa,LL);
 % for L=L_min:L_step:L_max
